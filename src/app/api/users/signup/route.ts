44ea7a799653cd54832450297c0cd4a185bf8ab9
escott2 <etriplett14@gmail.com>
2024-12-06 15:39:43 -0600

    add user model and schema

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,15 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
